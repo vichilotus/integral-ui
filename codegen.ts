7f9920e270ba24c32dc5f992ff4d268b8737a011
lilchizh <xlilchizhx@gmail.com>
2024-12-12 12:34:29 +0300

    bump SDK, change graphs

--- codegen.ts
+++ codegen.ts
@@ -3,9 +3,9 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
     overwrite: true,
     schema: [
-        'https://api.goldsky.com/api/public/project_cm2evmcbvx4b601wa8gp1f4du/subgraphs/analytics/1.0.0/gn',
-        'https://api.goldsky.com/api/public/project_cm2evmcbvx4b601wa8gp1f4du/subgraphs/blocklytics/1.0.0/gn',
-        'https://api.goldsky.com/api/public/project_cm2evmcbvx4b601wa8gp1f4du/subgraphs/farming/1.0.0/gn',
+        'https://api.studio.thegraph.com/query/97417/analytics/v0.0.1',
+        'https://api.studio.thegraph.com/query/97417/blocks/v0.0.1',
+        'https://api.studio.thegraph.com/query/97417/farmings/v0.0.1',
     ],
     documents: 'src/graphql/queries/!(*.d).{ts,tsx}',
     generates: {
